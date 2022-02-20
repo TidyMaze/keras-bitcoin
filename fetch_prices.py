0d2bd2e12317d4acd548a521f01983f725302cce
Yann Rolland <yann.rolland@iadvize.com>
2022-02-20 23:03:18 +0100

    wip

--- fetch_prices.py
+++ fetch_prices.py
@@ -9,4 +9,6 @@ from dateutil.parser import parse
 def load():
     data = load_csv('bitcoin_history_2021.csv')
     # print(data)
-    return PairHistory('btc/usdt', list(map(lambda item: PriceItem(datetime.strptime(item['Date'], '%d/%m/%Y'), float(item['Dernier'].replace(".", "").replace(",", "."))), data)))
+    history = [PriceItem(datetime.strptime(item['Date'], '%d/%m/%Y'), float(
+        item['Dernier'].replace(".", "").replace(",", "."))) for item in data]
+    return PairHistory('btc/usdt', history)
