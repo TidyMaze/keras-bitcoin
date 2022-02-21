bf1d9524f156c7a14780666178459bf0515a6e84
Yann Rolland <yann.rolland@iadvize.com>
2022-02-21 21:41:36 +0100

    wip

--- keras_bitcoin.py
+++ keras_bitcoin.py
@@ -81,6 +81,7 @@ print(len(predictedListItems))
 
 predictedPairHistory = PairHistory('predicted', predictedListItems)
 
+print(predictedPairHistory.history)
 print(len(predictedPairHistory.history))
 
 plotMultiPairHistory([yPairHistory, predictedPairHistory])
