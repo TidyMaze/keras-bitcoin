3ddc7691b9fa70f02123e75ebe76d516a4d03a23
Yann Rolland <yann.rolland@iadvize.com>
2022-02-21 22:32:18 +0100

    wip

--- keras_bitcoin.py
+++ keras_bitcoin.py
@@ -58,7 +58,7 @@ print(np.any(np.isnan(X)))
 print(np.any(np.isnan(y)))
 
 es = tf.keras.callbacks.EarlyStopping(
-    monitor='val_loss', min_delta=10000, patience=200, verbose=0, mode='auto')
+    monitor='val_loss', patience=200, verbose=0, mode='auto')
 
 history = model.fit(X, y, epochs=10000, batch_size=32,
                     validation_split=0.2, callbacks=[es], verbose=2)
