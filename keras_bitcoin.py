91873632c9ea600b00411109586b3b65e67d9506
Yann Rolland <yann.rolland@iadvize.com>
2022-02-21 01:01:22 +0100

    wip

--- keras_bitcoin.py
+++ keras_bitcoin.py
@@ -28,11 +28,11 @@ model = Sequential()
 model.add(tf.keras.Input(shape=(4,)))
 model.add(normalization_layer)
 model.add(Dense(16))
-model.add(Activation('sigmoid'))
+model.add(Activation('relu'))
 model.add(Dense(1))
 model.add(Activation('linear'))
 
-sgd = SGD(learning_rate=0.0001)
+sgd = SGD(learning_rate=0.01)
 model.compile(loss='mean_squared_error', optimizer=sgd)
 
 print(np.any(np.isnan(X)))
