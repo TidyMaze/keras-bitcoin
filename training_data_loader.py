94e6442b2e42d2e7eaa2248354d6c2c13342c094
Yann Rolland <yann.rolland@iadvize.com>
2022-02-27 14:07:13 +0100

    wip

--- training_data_loader.py
+++ training_data_loader.py
@@ -7,13 +7,8 @@ from processing import compute_with_fibo_history
 
 def load_training_data_for_regression() -> Tuple[np.ndarray, list[datetime]]:
     pair_history = load()
-    # lines = pairHistoryMovingAverageRange(pair_history, start=2, end=10, step=3)
     seq = compute_with_fibo_history(pair_history.history, 10)
     seq_with_only_prices = [[col.price for col in rows] for rows in seq]
-
-    # for i in range(len(seq_with_only_prices)):
-    # print(f'{i}: {seq_with_only_prices[i]}')
-
     train_data = np.array(seq_with_only_prices)
     dates = [col.timestamp for col in pair_history.history][-len(train_data):]
 
@@ -24,13 +19,8 @@ def load_training_data_for_regression() -> Tuple[np.ndarray, list[datetime]]:
 
 def load_training_data_for_classification() -> Tuple[np.ndarray, list[datetime]]:
     pair_history = load()
-    # lines = pairHistoryMovingAverageRange(pair_history, start=2, end=10, step=3)
     seq = compute_with_fibo_history(pair_history.history, 10)
     seq_with_only_prices = [[col.price for col in rows] for rows in seq]
-
-    # for i in range(len(seq_with_only_prices)):
-    # print(f'{i}: {seq_with_only_prices[i]}')
-
     train_data = np.array(seq_with_only_prices)
     dates = [col.timestamp for col in pair_history.history][-len(train_data):]
 
