5676aedf2e75d9e663e20b72b0a410d0f16c294e
Yann Rolland <yann.rolland@iadvize.com>
2022-03-09 00:54:57 +0100

    wip

--- keras_bitcoin_more_less.py
+++ keras_bitcoin_more_less.py
@@ -63,14 +63,14 @@ def run():
     model.add(Dense(1, activation='sigmoid'))
 
     # sgd = SGD(learning_rate=0.01, clipnorm=1.0)
-    adam = Adam(learning_rate=0.01, clipnorm=1.0)
+    adam = Adam(learning_rate=0.001, clipnorm=1.0)
     model.compile(loss='binary_crossentropy', optimizer=adam, metrics=['accuracy'])
 
     print(np.any(np.isnan(x)))
     print(np.any(np.isnan(y)))
 
     history: History = model.fit(x, y, epochs=500,
-                                 validation_split=0.2, verbose=2, batch_size=128)
+                                 validation_split=0.2, verbose=2, batch_size=16)
 
     print(history.history.keys())
 
