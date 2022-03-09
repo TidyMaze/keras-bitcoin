faa2ea44c4aebbce1422315ef7ad353d1c8e34be
Yann Rolland <yann.rolland@iadvize.com>
2022-03-09 01:35:05 +0100

    wip

--- keras_bitcoin_more_less.py
+++ keras_bitcoin_more_less.py
@@ -73,7 +73,7 @@ def run():
     print(np.any(np.isnan(x)))
     print(np.any(np.isnan(y)))
 
-    history: History = model.fit(x, y, epochs=100,
+    history: History = model.fit(x, y, epochs=500,
                                  validation_split=0.2, verbose=2, batch_size=128, shuffle=True)
 
     print(history.history.keys())
