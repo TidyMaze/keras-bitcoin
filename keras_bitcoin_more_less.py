22900e71762352a6d408e03c2d516dff4ecd4533
Yann Rolland <yann.rolland@iadvize.com>
2022-03-09 01:26:51 +0100

    wip

--- keras_bitcoin_more_less.py
+++ keras_bitcoin_more_less.py
@@ -49,7 +49,7 @@ def run():
 
     activation_fn = tf.keras.layers.LeakyReLU(alpha=0.3)
 
-    dropout = 0.05
+    # dropout = 0.5
     l2 = 0.001
     reg = regularizers.l2(l2)
     # reg = None
@@ -59,11 +59,11 @@ def run():
     model = Sequential()
     model.add(tf.keras.Input(shape=(18,), ))
     model.add(normalization_layer)
-    model.add(Dropout(dropout))
+    # model.add(Dropout(dropout))
     model.add(Dense(hidden_layer_size, activation=activation_fn, kernel_regularizer=reg))
-    model.add(Dropout(dropout))
+    # model.add(Dropout(dropout))
     model.add(Dense(hidden_layer_size, activation=activation_fn, kernel_regularizer=reg))
-    model.add(Dropout(dropout))
+    # model.add(Dropout(dropout))
     model.add(Dense(1, activation='sigmoid'))
 
     # sgd = SGD(learning_rate=0.01, clipnorm=1.0)
